c69b507922a66fcff53a6abb71c28236842f6f42
Soulkaku <tangogitlab@gmail.com>
2025-04-25 17:30:51 -0300

    no changes

--- SRC/Socket/SocketBack.js
+++ SRC/Socket/SocketBack.js
@@ -3,9 +3,11 @@ import io from "../../server.js";
 io.on("connection", (socket) => {
     console.log(socket.id + " connected");
 
-    socket.on("client-message", (content, room) => {
-        if(content != "") {
-            socket.to(room).emit("friend-message", content);
-        }
+    socket.on("client-message", message => {
+        var content = message.content
+        var room = message.room
+
+        socket.to(room).emit("friend-message", content);
+        
     });
 });
