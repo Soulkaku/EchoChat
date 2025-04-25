27e9ba646ee3655e7a6a7e06ee9956f4b0596f81
Soulkaku <tangogitlab@gmail.com>
2025-04-25 16:54:10 -0300

    agora recebe mensagens de todos os usuarios

--- Public/js/chat-script.js
+++ Public/js/chat-script.js
@@ -8,22 +8,23 @@ document.getElementById("username").textContent = user.username;
 
 const sendMessage = document.getElementById("button-sendMessage");
 
+socket.on("friend-message", messageContent => {
+    createMessage(messageContent, "friend-message");
+});
+
 sendMessage.addEventListener("click", (e) => {
     e.preventDefault();
 
    const getMessage = document.getElementById("messagePost");
-   
-   if(getMessage == "") {
-    alert("message is undefined");
-    return;
-   }
+    if(getMessage == "") {
+        alert("message is undefined");
+        return;
+    }
 
-   let room = defineRoom();
-
-   const message = {
-    content : getMessage.value.trim(),
-    room : room
-   }
+    const message = {
+        content : getMessage.value.trim(),
+        room : ""
+    }
 
    createMessage(message.content, "your-message");
 
@@ -47,18 +48,20 @@ function createMessage(content, person) {
     document.getElementById("chat-message").appendChild(messageDiv);
 }
 
-function defineRoom() {
-    const submitRoom = document.getElementById("define-room");
+// function defineRoom() {
+//     const submitRoom = document.getElementById("define-room");
 
-    submitRoom.addEventListener("click", (e) => {
-        e.preventDefault();
+//     submitRoom.addEventListener("click", (e) => {
+//         e.preventDefault();
 
-        const defineRoom = document.getElementById("roomInput");
-        const room = defineRoom.value.trim();
+//         const defineRoom = document.getElementById("roomInput");
+//         const room = defineRoom.value.trim();
         
 
-        if(room == "") {
-           return alert("log 2: undefined");
-        }
-    });
-}
+//         if(room == "") {
+//            alert("log 2: undefined");
+//         }
+
+//         alert("log 3: yes")
+//     });
+// }
