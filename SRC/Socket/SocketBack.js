bee8483c4b6ce1aded4210d9cdbf3940f8fe172c
Soulkaku <tangogitlab@gmail.com>
2025-04-25 16:33:04 -0300

    socket para enviar uma mensagem especifica para uma sala

--- SRC/Socket/SocketBack.js
+++ SRC/Socket/SocketBack.js
@@ -2,18 +2,10 @@ import io from "../../server.js";
 
 io.on("connection", (socket) => {
     console.log(socket.id + " connected");
-    
-    socket.on("verify-user", user => {
-        const { username } = user;
-        console.log(user); //verification log
-        
-        if( username == "") {
-            const message = "Type a name!";
-            socket.emit("reject-user", message);        
-        } else { 
-            socket.emit("pass-user", username);
-        }
-        //else if("name doesnt exist in data base");
 
+    socket.on("client-message", (content, room) => {
+        if(content != "") {
+            socket.to(room).emit("friend-message", content);
+        }
     });
 });
