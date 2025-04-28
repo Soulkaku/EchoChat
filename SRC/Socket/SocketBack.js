0ca613c69842b3d7c84df80a7beb4c371b27993b
Soulkaku <tangogitlab@gmail.com>
2025-04-28 16:16:35 -0300

    recebe a sala para enviar a mensagem

--- SRC/Socket/SocketBack.js
+++ SRC/Socket/SocketBack.js
@@ -3,11 +3,22 @@ import io from "../../server.js";
 io.on("connection", (socket) => {
     console.log(socket.id + " connected");
 
+    let room;
+
+    socket.on("enter-room", socketRoom => {
+        room = socketRoom;
+        socket.join(socketRoom);
+
+    });
+
+    
+ 
     socket.on("client-message", message => {
-        var content = message.content
-        var room = message.room
+        const content = message.content;
+        const roomMessage = message.room
 
         socket.to(room).emit("friend-message", content);
-        
+        console.log(content +  " " + room);
     });
+
 });
