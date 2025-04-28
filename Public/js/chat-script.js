f299b33012766928ece35c66858dcfbb43f87bdf
Soulkaku <tangogitlab@gmail.com>
2025-04-28 16:15:51 -0300

    recebe uma sala especifica para enviar mensagem, mas recebe mensagens da sala anterior ainda

--- Public/js/chat-script.js
+++ Public/js/chat-script.js
@@ -1,6 +1,5 @@
 import { socket } from "./socket.js";
 
-
 const user = JSON.parse(sessionStorage.getItem("user-client"));
 console.log(user);
 
@@ -11,6 +10,7 @@ document.getElementById("username").textContent = user.username;
 const sendMessage = document.getElementById("button-sendMessage");
 
 socket.on("friend-message", messageContent => {
+    console.log(room);
     createMessage(messageContent, "friend-message");
 });
 
@@ -25,15 +25,12 @@ sendMessage.addEventListener("click", (e) => {
 
     const message = {
         content : getMessage.value.trim(),
-        room : ""
+        room : room
     }
 
    createMessage(message.content, "your-message");
 
-   getMessage.textContent = "";
-
    socket.emit("client-message", message);
-
 });
 
 document.getElementById("define-room").addEventListener("click", (e) => {
@@ -45,11 +42,11 @@ document.getElementById("define-room").addEventListener("click", (e) => {
 
 function createMessage(content, person) {
     var messageDiv = document.createElement("div");
-    messageDiv.classList.add(person);
+        messageDiv.classList.add(person);
 
     var message = document.createElement("p");
-    message.classList.add("message");
-    message.textContent = content;
+        message.classList.add("message");
+        message.textContent = content;
 
     messageDiv.appendChild(message);
 
@@ -60,24 +57,7 @@ function defineRoom() {
     const roomInput = document.getElementById("roomInput");
     room = roomInput.value.trim();
 
-    roomInput.textContent = "null";
-    console.log("log 2: function defineRoom");
+    console.log("log 2: " + room);
+
+    socket.emit("enter-room", room);
 }
-
-// function defineRoom() {
-//     const submitRoom = document.getElementById("define-room");
-
-//     submitRoom.addEventListener("click", (e) => {
-//         e.preventDefault();
-
-//         const defineRoom = document.getElementById("roomInput");
-//         const room = defineRoom.value.trim();
-        
-
-//         if(room == "") {
-//            alert("log 2: undefined");
-//         }
-
-//         alert("log 3: yes")
-//     });
-// }
