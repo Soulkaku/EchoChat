49f9052cf6538d66dd41db0386378d4733fc6fa7
Soulkaku <tangogitlab@gmail.com>
2025-04-29 17:46:05 -0300

    inserção do user no model da mensagem

--- SRC/Models/messageModel.js
+++ SRC/Models/messageModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const messageModel = new mongoose.Schema({
 
     text : { type : String, required: true },
-    // user : { type : String, required: true },
+    user : { type : Object, required: true },
     origin : { type : String },
 
 }, { versionKey: false });
