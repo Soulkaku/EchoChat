15e07bb36f3feaa59d3df3dc98c8efbc0ace7f5c
Soulkaku <tangogitlab@gmail.com>
2025-04-30 15:07:30 -0300

    remoção da constante mensagem por não ser necessario ao usar o export default

--- SRC/Models/messageModel.js
+++ SRC/Models/messageModel.js
@@ -8,7 +8,4 @@ const messageModel = new mongoose.Schema({
 
 }, { versionKey: false });
 
-// const message = mongoose.model("chat", messageModel);
-// export default message ;
-
 export default mongoose.model("chat", messageModel);
