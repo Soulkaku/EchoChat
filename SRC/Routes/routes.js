0ea9ed337d948c2acac42f410c74d95eeaaaec13
Soulkaku <tangogitlab@gmail.com>
2025-05-02 16:43:56 -0300

    criação de uma rota post para criar usuario

--- SRC/Routes/routes.js
+++ SRC/Routes/routes.js
@@ -3,6 +3,7 @@ import controllers from "../Controllers/chatControllers.js";
 
 const routes = express.Router();
 
+routes.post(`/user/create`, controllers.createUser);
 routes.get(`/conversas`, controllers.pushMessages);
 // routes.post(`/conversas/post`, controllers.postMessage);
 
