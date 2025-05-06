3e41697f6e0d3cd21dc87bdd5f0434bf27a303c3
Soulkaku <tangogitlab@gmail.com>
2025-05-06 17:06:33 -0300

    a sala é enviada como devia para o controller e o envio da mensagem está quase pronto só falta definir o usuario dela

--- Public/js/chat-script.js
+++ Public/js/chat-script.js
@@ -8,45 +8,50 @@ document.getElementById("username").textContent = userData.name;
 const roomInput = document.getElementById("roomInput");
 const sendRoom = document.getElementById("button-sendRoom");
 
+let room;
+
 sendRoom.addEventListener("click", (e) => {
     e.preventDefault();
+    
+    if(room != roomInput.value) {
+        socket.emit("leave-room", room);
+        room = "";
+    }
+    
+    room = roomInput.value;
+    socket.emit("enter-room", room);
 
-    console.log(roomInput.value);
-
-    socket.emit("leave-room", roomInput.value.trim());
-
-    socket.emit("enter-room", roomInput.value.trim());
-
-    fetch(`/conversas/getMessages/${roomInput.value.trim()}`).then(async (data) => {
-        console.log( await data.json());
+    fetch(`/conversas/getMessages/${room}`).then(async (data) => {
+        console.log(await data.json());
     });
 });
+
 
 const messageInput = document.getElementById("messageInput");
 const sendMessage = document.getElementById("button-sendMessage");
 
-const message = {
-    text: messageInput.value.trim(),
-    room: roomInput.value.trim(),
-    user: userData
-}
-
 sendMessage.addEventListener("click", (e) => {
     e.preventDefault();
+
+    const message = {
+        text: messageInput.value.trim(),
+        room: room,
+        user: userData
+    }
 
     console.log(message);
 
     async function postMessage() {
         const reqBody = {
             method: "POST",
-            Headers: { "Content-Type" : "application/json" },
+            headers: { "Content-Type" : "application/json" },
             body: JSON.stringify(message)
         }
 
-        const request = await fetch("/conversas/postMessage", reqBody);
-        // request.then(async (response) => {
-        //     console.log(await response.json());
-        // });
+        const request = fetch("/conversas/postMessage", reqBody);
+        request.then(async (response) => {
+            console.log(await response.json());
+        });
     }
     postMessage();
 });
@@ -59,18 +64,18 @@ sendMessage.addEventListener("click", (e) => {
 // }); 
 
 
-// //FUNCTIONS
-// //~
-// function createMessage(message, person) {
+//FUNCTIONS
+//~
+function createMessage(message, person) {
     
-//     const messageDiv = document.createElement("div");
-//             messageDiv.classList.add(person);
+    const messageDiv = document.createElement("div");
+            messageDiv.classList.add(person);
 
-//     const messageP = document.createElement("p");
-//             messageP.classList.add("message");
-//             messageP.textContent = message.text;
+    const messageP = document.createElement("p");
+            messageP.classList.add("message");
+            messageP.textContent = message.text;
 
-//             messageDiv.appendChild(messageP);
+            messageDiv.appendChild(messageP);
 
-//     document.getElementById("chat-message").appendChild(messageDiv);
-// }
+    document.getElementById("chat-message").appendChild(messageDiv);
+}
