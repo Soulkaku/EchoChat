4611cd1824fef7d44168a1b2d07f389996fe47a5
Soulkaku <tangogitlab@gmail.com>
2025-05-07 15:09:49 -0300

    os ids das mensagens do usuario serão armazenados no conteudo do user

--- SRC/Models/userModel.js
+++ SRC/Models/userModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userModel = new mongoose.Schema({
     name: { type: String, required: true},
     //password: {type: String, required: true},
+   messages : [{ type : mongoose.Schema.Types.ObjectId, ref: "message" }]
 }, {versionKey : false });
 
 export default mongoose.model("user", userModel);
