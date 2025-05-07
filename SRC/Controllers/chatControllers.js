5ae6d656ec61c73d3eabbf22c050599c47945b87
Soulkaku <tangogitlab@gmail.com>
2025-05-07 17:35:24 -0300

    recebe a mensagem do usuario e é criada no banco

--- SRC/Controllers/chatControllers.js
+++ SRC/Controllers/chatControllers.js
@@ -11,7 +11,6 @@ class chatController {
 
             const newUser = await service.createUser({ name: name});
 
-
             if (!newUser) {
                 return res.status(400).json({message: "This name already exist in the database"});
             }
@@ -23,24 +22,41 @@ class chatController {
         }
     }
 
-    static async pushMessages(req, res) {
+    static async createMessage(req, res) {
         try {
-            const room = req.params.room;
-            const showMessages = await service.showMessages(room);
-            console.log(showMessages);
+            const { text, room, user } = req.body;
 
-            res.status(200).json(showMessages);
+            const createMessage = await service.createMessage(text, room, user);
+
+            res.status(200).json(createMessage);
         } catch (error) {
-            res.status(500).json({ messsage : `ERROR in pushing messages: ${error.message}`});
+            res.status(400).json({message: `Error in creating message: ${error.message} `});
         }
     }
 
-    static async postMessage(req, res) {
-        const { text, room, user } = req.body;
+    // static async pushMessages(req, res) {
+    //     try {
+    //         const room = req.params.room;
+    //         const showMessages = await service.showMessages(room);
+    //         console.log(showMessages);
 
-        const postMessage = await service.postMessage(text, room, user);
+    //         res.status(200).json(showMessages);
+    //     } catch (error) {
+    //         res.status(500).json({ messsage : `ERROR in pushing messages: ${error.message}`});
+    //     }
+    // }
 
-    }
+    // static async createMessage(req, res) {
+    //     try {
+    //         const { text, room, user } = req.body;
+
+    //         const createMessage = await service.createMessage(text, room, user);
+    //         console.log(createMessage);
+
+    //     } catch(error) {
+    //         res.status(400).json({ message : `ERROR in creating your message: ${error.message}`});
+    //     }
+    // }
 }
 
 export default chatController;
