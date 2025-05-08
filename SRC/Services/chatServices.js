9a36a6b79f8321ef247915dd38ea55e394e5154e
Soulkaku <tangogitlab@gmail.com>
2025-05-08 16:11:40 -0300

    função de puxar mensagens de uma sala criada: retornando para o front como devia

--- SRC/Services/chatServices.js
+++ SRC/Services/chatServices.js
@@ -17,6 +17,7 @@ class chatService {
     async createMessage(text, room, user) {
         const message = await messageModel.create({ text: text, room: room, user: user });
         const userId = user._id
+
         async function pushMessageToUser() {
             const updatedUser = await userModel.findByIdAndUpdate(userId, { $push : { messages : message._id }}, { new : true });
             return updatedUser;
@@ -25,6 +26,16 @@ class chatService {
         pushMessageToUser();
         return message;        
     }
+
+    async pushMessages(room) {
+        const pushMessagesFromRoom = await messageModel.find({ room: room }).select('-_id text user');
+        return pushMessagesFromRoom.map(char =>( {
+            text : char.text,
+            user : char.user
+        }));
+    }
+
+
 }
 
 export default new chatService;
