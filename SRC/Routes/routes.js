37a65dba31d407cbc3c5485939cf150d5365775e
Soulkaku <tangogitlab@gmail.com>
2025-05-09 17:34:03 -0300

    aplicação dos validadores de chat em suas devidas rotas

--- SRC/Routes/routes.js
+++ SRC/Routes/routes.js
@@ -2,13 +2,13 @@ import express from "express";
 import chatController from "../Controllers/chatControllers.js";
 import loginController from "../Controllers/userControllers.js";
 import loginValidator from "../Validators/login-validation.js";
-
+import chatValidator from "../Validators/chat-validation.js";
 
 const routes = express.Router();
 
 routes.post(`/user/create`, [...loginValidator.validateName()], loginController.createUser);
 
-routes.get(`/conversas/getMessages/:room`, chatController.pullMessages, );
-routes.post(`/conversas/createMessage`, chatController.createMessage);
+routes.get(`/conversas/getMessages/:room`, [...chatValidator.validateRoom] , chatController.pullMessages, );
+routes.post(`/conversas/createMessage`, [...chatValidator.validateTextMessage] , chatController.createMessage);
 
 export { routes };
