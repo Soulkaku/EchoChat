4f10a105122d698c6e7c2a242aa2ad30ad2e98d8
Soulkaku <tangogitlab@gmail.com>
2025-05-09 15:38:12 -0300

    tudo relacionado a login de usuario foi removido

--- SRC/Controllers/chatControllers.js
+++ SRC/Controllers/chatControllers.js
@@ -4,24 +4,6 @@ import service from "../Services/chatServices.js";
 // const service = new chatService;
 
 class chatController {
-
-    static async createUser(req, res) {
-        try {
-            const { name } = req.body;
-
-            const User = await service.createUser({ name: name});
-
-            // if (!User) {
-            //     return res.status(400).json({message: "This name already exist in the database"});
-            // }
-            
-            res.status(201).json(User);
-        } catch (error) {
-            res.status(500).json({ message : `ERROR in create user: ${error.message}`});
-            console.error(error);
-        }
-    }
-
     static async createMessage(req, res) {
         try {
             const { text, room, user } = req.body;
@@ -46,6 +28,7 @@ class chatController {
             console.log(messages);
 
             res.status(200).json(messages);
+            
         } catch (error) {
             res.status(500).json({ message: `ERROR in pushing messages from a room: ${error.message}`});
         }
