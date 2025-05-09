76667eac278a98761a65cd3493e7578925f4f9f8
Soulkaku <tangogitlab@gmail.com>
2025-05-09 15:37:19 -0300

    alteração na importação de controllers

--- SRC/Routes/routes.js
+++ SRC/Routes/routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import controllers from "../Controllers/chatControllers.js";
+import chat from "../Controllers/chatControllers.js";
+import login from "../Controllers/userControllers.js";
 
 const routes = express.Router();
 
-routes.post(`/user/create`, controllers.createUser);
-routes.get(`/conversas/getMessages/:room`, controllers.pullMessages);
-routes.post(`/conversas/createMessage`, controllers.createMessage);
+routes.post(`/user/create`, login.createUser);
 
+routes.get(`/conversas/getMessages/:room`, chat.pullMessages);
+routes.post(`/conversas/createMessage`, chat.createMessage);
 
 export { routes };
