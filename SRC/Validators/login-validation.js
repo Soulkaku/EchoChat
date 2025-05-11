fbd8ce7cf81358c6b2c409ce0f899e970b7df937
Soulkaku <tangogitlab@gmail.com>
2025-05-11 13:04:09 -0300

    fix: result is now called errors

--- SRC/Validators/login-validation.js
+++ SRC/Validators/login-validation.js
@@ -9,7 +9,7 @@ class loginValidators {
             (req, res, next) => {
                 const errors = validationResult(req);
 
-                if(!result.isEmpty()) {
+                if(!errors.isEmpty()) {
                     return res.status(400).json({ errors : errors.array() });
                 }
                 next();
